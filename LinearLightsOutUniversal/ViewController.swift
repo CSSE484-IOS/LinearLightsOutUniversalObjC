8f0521df0f716daac1a8c1ba664240d4d8b81934
Yizhi Feng <fengy2@rose-hulman.edu>
2018-03-19 21:11:11 -0400

    done

--- LinearLightsOutUniversal/ViewController.swift
+++ LinearLightsOutUniversal/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var gameStateLabel: UILabel!
+    @IBOutlet weak var gameStateNavBar: UINavigationBar!
+    @IBOutlet var gameBoardBtns: [UIButton]!
+    
+    var game = LinearLightsOutGame(numLights: 13)
+    var hasWon = false;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        gameStateNavBar.titleTextAttributes = [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 20)]
+        updateView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pressedNewGameBtn(_ sender: Any) {
+        game = LinearLightsOutGame(numLights: 13)
+        hasWon = false;
+        updateView()
     }
-
-
+    
+    @IBAction func pressedGameBoardBtns(_ sender: Any) {
+        let gameBoardBtn = sender as! UIButton
+        print(gameBoardBtn.tag)
+        hasWon = game.pressedLightAtIndex(gameBoardBtn.tag)
+        updateView()
+    }
+    
+    func updateView() {
+        if traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.compact {
+            gameStateLabel.text = hasWon ? "Won in \(game.movesTaken) moves!!!" : "Moves Taken: \(game.movesTaken)"
+        } else {
+            gameStateNavBar.topItem?.title = hasWon ? "Won in \(game.movesTaken) moves!!!" : "Moves Taken: \(game.movesTaken)"
+        }
+        
+        for i in 0..<13 {
+            let btn = gameBoardBtns[i]
+            if game.lightStates[i] {
+                if hasWon {
+                    btn.setImage(#imageLiteral(resourceName: "light_on"), for: UIControlState.disabled)
+                } else {
+                    btn.setImage(#imageLiteral(resourceName: "light_on"), for: UIControlState.normal)
+                }
+            } else {
+                btn.setImage(#imageLiteral(resourceName: "light_off"), for: UIControlState.normal)
+            }
+        }
+    }
+    
 }
 
